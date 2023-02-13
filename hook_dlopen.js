bf15f2c0203b65e525c2c2388b52c88b09f252ff
马到成功 <82955884+mdcgqq@users.noreply.github.com>
2023-02-13 11:32:30 +0800

    Create hook_dlopen.js

---
+++ hook_dlopen.js
@@ -0,0 +1,41 @@
+function hook_dlopen(soName = '') {
+    Interceptor.attach(Module.findExportByName(null, "dlopen"),
+        {
+            onEnter: function (args) {
+                var pathptr = args[0];
+                if (pathptr !== undefined && pathptr != null) {
+                    var path = ptr(pathptr).readCString();
+                    // console.log("load " + path);
+                    if (path.indexOf(soName) >= 0) {
+                        this.is_can_hook = true;
+                    }
+                }
+            },
+            onLeave: function (retval) {
+                if (this.is_can_hook) {
+                    // do something
+                }
+            }
+        }
+    );
+ 
+    Interceptor.attach(Module.findExportByName(null, "android_dlopen_ext"),
+        {
+            onEnter: function (args) {
+                var pathptr = args[0];
+                if (pathptr !== undefined && pathptr != null) {
+                    var path = ptr(pathptr).readCString();
+                    // console.log("load " + path);
+                    if (path.indexOf(soName) >= 0) {
+                        this.is_can_hook = true;
+                    }
+                }
+            },
+            onLeave: function (retval) {
+                if (this.is_can_hook) {
+                    // do something
+                }
+            }
+        }
+    );
+}
